9d17ead9699b17ed26aad1b5085a9dbc209bef93
Julián <julianbrionesgtz15@gmail.com>
2026-07-24 03:12:38 -0600

    Delete Promedio.py

--- Promedio.py
+++
@@ -1,5 +0,0 @@
-num1 = float(input("Ingrese el número 1: "))
-num2 = float(input("Ingrese el número 2: "))
-num3 = float(input("Ingrese el número 3: "))
-promedio = (num1+num2+num3)/3
-print("El promedio es: ", promedio)
